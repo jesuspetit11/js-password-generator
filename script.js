fa0bccf25a0b726ee01090d8abf11ef1066470d5
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-12-27 21:38:16 -0400

    project finished, waiting for more features coming soon

---
+++ script.js
@@ -0,0 +1,38 @@
+const passBox = document.querySelector("#password");
+const btnCrearPassword = document.querySelector(".btnPass");
+const copyPass = document.querySelector("img");
+console.log(copyPass);
+
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()-=_+[]{}";
+
+//Un string con todos las variables anteriores
+const allChars = upperCase + lowerCase + numbers + symbols;
+
+//addEventListeners
+btnCrearPassword.onclick = createPassword;
+copyPass.onclick = copyPassword;
+
+//Funciones
+function createPassword() {
+    let password = "";
+    //Toma un caracter de cualquiera de los strings aleatoriamente
+    //Ya que multiplicamos 
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(length >= password.length){ //Mientras que password.length no tenda 12, seguimos añadiendo caracteres
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passBox.value = password; //
+}
+
+function copyPassword() {
+    let password = passBox.value;
+    navigator.clipboard.writeText(password);
+}
